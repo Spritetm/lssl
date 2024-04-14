6069842d09c44ed4051bfbfd75cea757e8869716
Jeroen Domburg <git@j0h.nl>
2024-04-14 12:14:09 +0800

    Syntax highlighting

--- index.js
+++ index.js
@@ -3,13 +3,50 @@ window.addEventListener("load", (event) => {
 	const btn = document.querySelector("#compile_btn");
 	btn.disabled=true;
 	btn.addEventListener("click", recompile);
+	const btntst = document.querySelector("#test_btn");
+	btntst.addEventListener("click", test);
 });
+
+
+function test() {
+	classtxt=[
+		"", //unknown, never returned by code
+		"oper",
+		"langfunc",
+		"number",
+		"punctuation",
+		"comment",
+	];
+	const code = document.querySelector("#code_text").textContent;
+	var ptr=Module.ccall('tokenize_for_syntax_hl', 'number', ['string'], [code]);
+	var txtpos=0;
+	var hltext=new Array();
+	while(1) {
+		var r=Module.HEAPU32.subarray(ptr/4, ptr/4+16);
+		if (r[0]>r[1]) break; //end sentinel
+		if (r[0]>txtpos) {
+			var txtnode=new Text(code.substring(txtpos, r[0]));
+			hltext.push(txtnode);
+		}
+		var span=document.createElement('span');
+		span.classList.add(classtxt[r[2]]);
+		var txtnode=new Text(code.substring(r[0], r[1]));
+		span.appendChild(txtnode);
+		hltext.push(span);
+		txtpos=r[1];
+		ptr+=16;
+	}
+	//add any remaining text
+	var txtnode=new Text(code.substring(txtpos));
+	hltext.push(txtnode);
+	document.querySelector("#code_text").replaceChildren(...hltext);
+}
+
 
 var led_time=0;
 
 function recompile() {
-	const code = document.querySelector("#code_text").value;
-	var len=0;
+	const code = document.querySelector("#code_text").textContent;
 	Module.ccall('recompile', 'Uint8Array', ['string'], [code]);
 	if (led_time==0) led_tick(); //kick off program run
 }
--- js_funcs.c
+++ js_funcs.c
@@ -8,7 +8,6 @@
 #include "led_syscalls.h"
 #include "vm.h"
 
-
 static lssl_vm_t *vm=NULL;
 static uint8_t *program;
 
@@ -53,4 +52,75 @@ uint8_t *get_led(int pos, float t) {
 	led_syscalls_get_rgb(&rgb[0], &rgb[1], &rgb[2]);
 	return rgb;
 }
+
+
+#define CLASS_UNK 0
+#define CLASS_OPER 1
+#define CLASS_LANGFUNC 2
+#define CLASS_NUMBER 3
+#define CLASS_PUNCTUATION 4
+#define CLASS_COMMENT 5
+
+//Mapping between classes and tokens for syntax highlighting
+int token_class[]={
+	[TOKEN_EOL]=CLASS_COMMENT, //should technically get its own class...
+	[TOKEN_PLUS]=CLASS_OPER, [TOKEN_MINUS]=CLASS_OPER, [TOKEN_SLASH]=CLASS_OPER, [TOKEN_TIMES]=CLASS_OPER, 
+	[TOKEN_LPAREN]=CLASS_PUNCTUATION, [TOKEN_RPAREN]=CLASS_PUNCTUATION,
+	[TOKEN_SEMICOLON]=CLASS_PUNCTUATION, [TOKEN_COMMA]=CLASS_PUNCTUATION,
+	[TOKEN_PERIOD]=CLASS_PUNCTUATION, [TOKEN_ASSIGN]=CLASS_PUNCTUATION,
+	[TOKEN_EQ]=CLASS_OPER, [TOKEN_NEQ]=CLASS_OPER, [TOKEN_L]=CLASS_OPER, [TOKEN_G]=CLASS_OPER, 
+	[TOKEN_LEQ]=CLASS_OPER, [TOKEN_GEQ]=CLASS_OPER,
+	[TOKEN_CURLOPEN]=CLASS_PUNCTUATION, [TOKEN_CURLCLOSE]=CLASS_PUNCTUATION,
+	[TOKEN_NUMBERF]=CLASS_NUMBER, [TOKEN_NUMBERI]=CLASS_NUMBER, 
+	[TOKEN_VAR]=CLASS_LANGFUNC, [TOKEN_FOR]=CLASS_LANGFUNC, 
+	[TOKEN_WHILE]=CLASS_LANGFUNC, [TOKEN_IF]=CLASS_LANGFUNC, 
+	[TOKEN_FUNCTION]=CLASS_LANGFUNC, [TOKEN_RETURN]=CLASS_LANGFUNC, 
+};
+
+typedef struct {
+	uint32_t start;
+	uint32_t end;
+	uint32_t class;
+	uint32_t pad;
+} token_t;
+
+token_t *tokenize_for_syntax_hl(char *code) {
+	yyscan_t myscanner;
+	yylex_init(&myscanner);
+	yy_scan_string(code, myscanner);
+
+	int tsz=1024;
+	int tlen=0;
+	token_t *tokens=calloc(tsz, sizeof(token_t));
+
+	int token;
+	union YYSTYPE tokeninfo={};
+	YYLTYPE loc={};
+	while ((token=yylex(&tokeninfo, &loc, myscanner))) {
+		if (token<(sizeof(token_class)/sizeof(token_class[0]))) {
+			int class=token_class[token];
+			if (class!=CLASS_UNK) {
+				if (tsz==tlen+1) {
+					tsz+=1024;
+					tokens=realloc(tokens, tsz*sizeof(token_t));
+				}
+				tokens[tlen].start=loc.pos_start;
+				tokens[tlen].end=loc.pos_end;
+				tokens[tlen].class=class;
+				tokens[tlen].pad=token_class[token];
+				tlen++;
+			}
+		}
+	}
+	//end marker
+	tokens[tlen].start=1;
+	tokens[tlen].end=0;
+
+//	yy_delete_buffer(YY_CURRENT_BUFFER, myscanner);
+	yylex_destroy(myscanner);
+
+	return tokens;
+}
+
+
 
